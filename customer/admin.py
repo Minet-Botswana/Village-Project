b9e0ee30095f789424b8c346fe80f1910af936c0
--local <lebaneaustin2@gmail.com>
2023-11-15 11:17:52 +0200

    Iinitial Commit

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,20 @@
+# app_name/admin.py
 from django.contrib import admin
+from django.utils.html import format_html
+from django.conf import settings
+from .models import Customer
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('get_name', 'address', 'mobile', 'display_profile_pic')
+
+    def display_profile_pic(self, obj):
+        if obj.profile_pic:
+            image_url = '{}{}'.format(settings.STATIC_URL, obj.profile_pic.url)
+            return format_html('<img src="{}" style="max-width: 100px; max-height: 100px;" />', image_url)
+        else:
+            return 'No Image'
+
+    display_profile_pic.allow_tags = True
+    display_profile_pic.short_description = 'Profile Picture'
+
+admin.site.register(Customer, CustomerAdmin)
