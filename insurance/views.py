e6f8e92fa686f83e2929e3443cfb462d5504e647
--local <lebaneaustin2@gmail.com>
2023-11-22 13:37:06 +0200

    EDIT: ENABLED THAT PRO-PICS RETAIN THEMSELVES

--- insurance/views.py
+++ insurance/views.py
@@ -63,28 +63,16 @@ def admin_view_customer_view(request):
     customers= CMODEL.Customer.objects.filter(user__groups__name='CUSTOMER')
     return render(request,'insurance/admin_view_customer.html',{'customers':customers})
 
-#@login_required(login_url='adminlogin')
-#def update_customer_view(request,pk):
-    #customer=CMODEL.Customer.objects.get(id=pk)
-    #user=CMODEL.User.objects.get(id=customer.user_id)
-    #userForm=CFORM.CustomerUserForm(instance=user)
-    #customerForm=CFORM.CustomerForm(request.FILES,instance=customer)
-    #mydict={'userForm':userForm,'customerForm':customerForm}
-    #if request.method=='POST':
-        #userForm=CFORM.CustomerUserForm(request.POST,instance=user)
-        #customerForm=CFORM.CustomerForm(request.POST,request.FILES,instance=customer)
-        #if userForm.is_valid() and customerForm.is_valid():
-            #user=userForm.save()
-            #user.set_password(user.password)
-            #user.save()
-            #customerForm.save()
-            #return redirect('admin-view-customer')
-    #return render(request,'insurance/update_customer.html',context=mydict)z
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+# Your existing imports...
+
 @login_required(login_url='adminlogin')
 def update_customer_view(request, pk):
     customer = CMODEL.Customer.objects.get(id=pk)
     user = CMODEL.User.objects.get(id=customer.user_id)
-    
+
     if request.method == 'POST':
         userForm = CFORM.CustomerUserForm(request.POST, instance=user)
         customerForm = CFORM.CustomerForm(request.POST, request.FILES, instance=customer)
@@ -98,6 +86,11 @@ def update_customer_view(request, pk):
 
             user.save()
 
+            # Check if profile_pic is empty in the form data
+            if not request.FILES.get('profile_pic'):
+                # If empty, set profile_pic to the current value from the database
+                customerForm.instance.profile_pic = customer.profile_pic
+
             # Save the customer form
             customerForm.save()
 
@@ -108,7 +101,6 @@ def update_customer_view(request, pk):
 
     mydict = {'userForm': userForm, 'customerForm': customerForm}
     return render(request, 'insurance/update_customer.html', context=mydict)
-
 
 
 
