9982a4004699d264bf8c373666c1cd39bd4870f2
Goitsemang Lebane <150056797+goitsemang-minet@users.noreply.github.com>
2024-02-12 11:00:22 +0200

    REMOVED PRO_PICS VARIABLE FROM VIEWS AS IT WAS REMOVED FROM THE MODEL

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'insurancemanagement.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 #Development Database
 DATABASES = {
     'default': {
@@ -114,7 +114,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
